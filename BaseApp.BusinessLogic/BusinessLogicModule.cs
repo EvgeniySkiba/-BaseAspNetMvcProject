2aaf4d58972240cfa43632928d91087bb834cb1a
skiba.evgeniy <skiba.Evgeniy@hotmail.com>
2017-08-18 18:55:21 +0300

    added buisiness logic classes

---
+++ BaseApp.BusinessLogic/BusinessLogicModule.cs
@@ -0,0 +1,19 @@
+﻿using BaseApp.BusinessLogic.Abstract;
+using BaseApp.BusinessLogic.Concrete;
+using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseApp.BusinessLogic
+{
+    public class BusinessLogicModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<ClientBusinessLogicInterface>().To<ClientBusinessLogic>();
+        }
+    }
+}
---
+++ BaseApp.BusinessLogic/Concrete/ClientBusinessLogic.cs
@@ -0,0 +1,21 @@
+﻿using BaseApp.BusinessLogic.Abstract;
+using BaseAsp.DAL.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseApp.BusinessLogic.Concrete
+{
+    public class ClientBusinessLogic : GenericBusinessLogic, ClientBusinessLogicInterface
+    {
+        private readonly IAccountTypeRepository _accountRepository;
+
+
+        public ClientBusinessLogic(IAccountTypeRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+    }
+}
